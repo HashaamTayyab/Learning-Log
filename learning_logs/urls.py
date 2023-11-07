8f4b9ec1a21e5f6347a1146f106d53461471b475
HashaamTayyab <59329329+HashaamTayyab@users.noreply.github.com>
2023-11-07 19:29:56 +0500

    Added templates to output the topics and their entries.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
+    # Home Page
     path('', views.index, name = 'index'),
+    # Page that shows all the topics.
+    path('topics/', views.topics, name = 'topics'),
+    path('topics/<int:topic_id>', views.topic, name = 'topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .models import Topic
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Shows all Topics in the database."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(pk = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries' : entries}
+    return render(request, 'learning_logs/topic.html', context)
